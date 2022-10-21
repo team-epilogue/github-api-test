464d82655a4674068a884ea1f527e5d7a3b0a4a3
yunaghgh <yunaghgh@naver.com>
2022-10-21 11:44:43 +0900

    [BE]feat: repository 선택

--- test-vue/src/components/FileUpdate.vue
+++ test-vue/src/components/FileUpdate.vue
@@ -1,27 +1,38 @@
 <template>
   <div>
     <h1>File update</h1>
-    <label for="repo">레포지토리 :</label>
-    <input id="repo" type="text" />
-    <!-- <button class="button" @click="fileupload">repository 선택</button> -->
+    <div v-if="repoList">
+      <label for="repo">레포지토리 :</label>
+      <ul id="repo">
+        <li v-for="(item, index) in repoList" v-bind:key="index" @click="selectRepo(item)">
+          <div>{{ item }}</div>
+        </li>
+        <!-- <button class="button" @click="select">선택</button> -->
+      </ul>
+      <!-- <input id="repo" type="text" /> -->
+      <!-- <button class="button" @click="fileupload">repository 선택</button> -->
+      <div v-if="selectedRepo">
+        <!-- <button class="button" @click="fileupload">repository 선택</button> -->
+        <br />
+        <label for="dir">폴더 경로 :</label>
+        <input id="dir" type="text" />
+        <!-- <button class="button" @click="fileupload">폴더 경로</button> -->
+        <button class="button" @click="check">조회</button>
 
-    <label for="dir">폴더 경로 :</label>
-    <input id="dir" type="text" />
-    <!-- <button class="button" @click="fileupload">폴더 경로</button> -->
-    <button class="button" @click="check">조회</button>
+        <ul v-if="files" id="filedir">
+          <li v-for="(item, index) in files" v-bind:key="index" @click="select(item)">
+            <div>{{ item }}</div>
+          </li>
+          <!-- <button class="button" @click="select">선택</button> -->
+        </ul>
 
-    <ul v-if="files">
-      <li v-for="(item, index) in files" v-bind:key="index" @click="select(item)">
-        <div>{{ item }}</div>
-      </li>
-      <!-- <button class="button" @click="select">선택</button> -->
-    </ul>
+        <div v-if="selected">
+          <label for="content">내용 :</label>
+          <textarea id="content" rows="8" cols="85" v-model="content"></textarea>
 
-    <div v-if="selected">
-      <label for="content">내용 :</label>
-      <textarea id="content" rows="8" cols="85" v-model="content"></textarea>
-
-      <button class="button" @click="update">업데이트</button>
+          <button class="button" @click="update">업데이트</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,8 +45,10 @@ export default {
     return {
       token: "",
       id: "",
-      files: [],
       selected: "",
+      files: [],
+      selectedRepo: "",
+      repoList: [],
       content: "",
       sha: "",
     };
@@ -44,16 +57,32 @@ export default {
     if (localStorage.getItem("token") && localStorage.getItem("id")) {
       this.token = localStorage.getItem("token");
       this.id = localStorage.getItem("id");
+      axios({
+        url: `https://api.github.com/users/${this.id}/repos`,
+        method: "get",
+      })
+        .then(({ data }) => {
+          data.forEach((value) => {
+            this.repoList.push(value.name);
+          });
+        })
+        .catch(() => {
+          alert("레포 리스트 로드 실패");
+          this.$router.push("/");
+        });
     } else {
       this.$router.push("/");
     }
   },
   methods: {
+    selectRepo(item) {
+      this.selectedRepo = item;
+    },
     check() {
       this.files = [];
       this.selected = "";
       axios({
-        url: `https://api.github.com/repos/${this.id}/${document.getElementById("repo").value}/contents/${document.getElementById("dir").value}`,
+        url: `https://api.github.com/repos/${this.id}/${this.selectedRepo}/contents/${document.getElementById("dir").value}`,
         method: "get",
       })
         .then(({ data }) => {
@@ -70,7 +99,7 @@ export default {
     select(item) {
       this.selected = item;
       axios({
-        url: `https://api.github.com/repos/${this.id}/${document.getElementById("repo").value}/contents/${document.getElementById("dir").value}/${this.selected}`,
+        url: `https://api.github.com/repos/${this.id}/${this.selectedRepo}/contents/${document.getElementById("dir").value}/${this.selected}`,
         method: "get",
       })
         .then(({ data }) => {
@@ -84,7 +113,7 @@ export default {
     update() {
       let content = window.btoa(unescape(encodeURIComponent(this.content)));
       axios({
-        url: `https://api.github.com/repos/${this.id}/${document.getElementById("repo").value}/contents/${document.getElementById("dir").value}/${this.selected}`,
+        url: `https://api.github.com/repos/${this.id}/${this.selectedRepo}/contents/${document.getElementById("dir").value}/${this.selected}`,
         method: "put",
         headers: {
           Authorization: `Bearer ${this.token}`,
@@ -97,6 +126,7 @@ export default {
       })
         .then(() => {
           alert("업데이트 성공");
+          this.$router.push("/");
         })
         .catch(() => {
           alert("업데이트 실패");
@@ -110,13 +140,16 @@ export default {
 ul {
   list-style-type: none;
   background-color: #ccc;
-  width: 180px;
   padding: 0;
   margin: auto;
   margin-top: 10px;
 }
 
-li div {
+#repo {
+  width: 500px;
+}
+
+#repo li div {
   text-decoration: none;
   display: block;
   color: #000;
@@ -125,7 +158,25 @@ li div {
   border-bottom: 1px solid #fff;
 }
 
-li div:hover {
+#repo li div:hover {
+  background-color: #c7df83;
+  color: #fff;
+}
+
+#filedir {
+  width: 200px;
+}
+
+#filedir li div {
+  text-decoration: none;
+  display: block;
+  color: #000;
+  padding: 8px 15px 8px 15px;
+  font-weight: bold;
+  border-bottom: 1px solid #fff;
+}
+
+#filedir li div:hover {
   background-color: tomato;
   color: #fff;
 }
